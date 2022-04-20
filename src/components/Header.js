bb60b70a68fa44519bccffd0b5cf488614159fb4
engibaryan <xachik.engibar@gmail.com>
2022-04-20 15:21:05 +0400

    react_task1

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import Button from "./Button.js";
+
+const Header = ({ title, onAdd, showAdd }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button 
+      color={showAdd ? "red" : "green" }
+      text={showAdd ? "Close" : "Add"} 
+      onClick={onAdd} 
+      />
+    </header>
+  );
+};
+Header.defaultProps = {
+  title: "Tast Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
+};
+
+//CSS in JS
+// const headingStyle = {
+//   color: "red",
+//   backgroundColor: "black",
+// };
+
+export default Header;
